2a559a525444374db087fed2e82f000f35439908
Taufik Pragusga <taufik@pragusga.com>
2024-06-26 19:47:59 +0700

    feat: vite port

---
+++ ecosystem.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'fe',
+      script: 'npm',
+      args: 'run dev',
+      instances: '1',
+      exec_mode: 'fork', // if there is only one instance, use cluster otherwise
+    },
+  ],
+};
