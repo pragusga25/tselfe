6a729d609ebded24b9e916d4f8444ce2395089d4
Taufik Pragusga <taufik@pragusga.com>
2024-07-16 16:11:32 +0700

    feat: Add error handling to useLogin hook

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -3,6 +3,7 @@ import { LoginPayload } from '@/types';
 import { login } from '@/api';
 import { useAuth } from '@/hooks';
 import toast from 'react-hot-toast';
+import { AxiosError } from 'axios';
 
 export const useLogin = () => {
   const { setAuth, setIsLoading } = useAuth();
@@ -14,18 +15,27 @@ export const useLogin = () => {
       setIsLoading(true);
       return login(payload);
     },
-    onMutate: () => {
-      toast.loading('Logging in...');
-    },
+
     onSuccess: (data) => {
       setAuth(data);
       localStorage.setItem('accessToken', data.accessToken);
       queryClient.clear();
       window.location.replace('/');
       setIsLoading(false);
+      toast.success('Login successful');
     },
-    onError: () => {
+    onError: (err) => {
       setIsLoading(false);
+      if (err instanceof AxiosError) {
+        const errorDetails = err.response?.data?.error?.details?.[0];
+
+        if (errorDetails) {
+          toast.error(errorDetails);
+          return;
+        }
+      }
+
+      toast.error('An error occurred. Please try again.');
     },
   });
 
